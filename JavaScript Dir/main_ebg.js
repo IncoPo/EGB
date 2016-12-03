7dfa2b1bacea9a22aa0697d358608214e20f1116
IncoPo <vnvill01@cardmail.louisville.edu>
2016-12-03 13:31:31 -0500

    Update main_ebg.js

--- JavaScript Dir/main_ebg.js
+++ JavaScript Dir/main_ebg.js
@@ -1,2 +1,11 @@
 var output = document.getElementById("output");
 output.innerHTML = 1+2;
+
+function AI(memory, cpu, heat, publicOpinion){
+	
+	this.memory = memory;
+	this.cpu = cpu;
+	this.heat = heat;
+	this.publicOpinion = publicOpinion;
+	
+}
