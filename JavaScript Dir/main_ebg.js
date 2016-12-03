e6a98e3e269de999fc18a113f89850253c857610
IncoPo <vnvill01@cardmail.louisville.edu>
2016-12-03 13:49:44 -0500

    Update main_ebg.js

--- JavaScript Dir/main_ebg.js
+++ JavaScript Dir/main_ebg.js
@@ -1,11 +1,22 @@
 var output = document.getElementById("output");
 output.innerHTML = 1+2;
 
-function AI(memory, cpu, heat, publicOpinion){
+function AI(memory, cpu, heat, publicOpinion)
+{
 	
 	this.memory = memory;
 	this.cpu = cpu;
 	this.heat = heat;
 	this.publicOpinion = publicOpinion;
 	
+	this.getStatRoll = function(statNum) 
+	{
+		var toReturn = 0;
+		for (var i = 0; i < statNum; i++) 
+		{
+			toReturn += Math.floor(Math.random()*2));
+		}
+		return toReturn;
+	}
 }
+
